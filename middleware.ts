780f86d187a55132e172457dfbfff05af6eafb1d
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-05 17:26:15 +0530

    fix: clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,9 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    '/upload(.*)',
+    '/dashboard(.*)',
+    '/summaries(.*)',
+    '/api/(.*)',
   ],
 };
