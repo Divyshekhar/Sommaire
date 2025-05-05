688c89c6ca1f42fb31ac751d1c4aeedec86a642e
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-05 17:33:09 +0530

    fix: remove unwanted files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import DemoSection from '@/components/home/demo-section';
 import HeroSection from '../components/home/hero-section';
 import BgComponent from '@/components/common/bg-gradient';
 import HowItWorksSection from '@/components/home/how-it-works-section';
-import PricingSection from '@/components/home/pricing-section';
 import CTASection from '@/components/home/cta-section';
 export default function Home() {
   return (
