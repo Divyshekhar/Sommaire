0b55a31856c55ad12d275c38167c82629a3f2c19
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-05 17:17:58 +0530

    fix: clerk middleware

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -98,9 +98,11 @@ const UploadForm = () => {
 
 
   return (
-    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
-      <UploadFormInput ref={formRef} onSubmit={handleSubmit} isLoading={loading} />
-    </div>
+    <>
+      <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+        <UploadFormInput ref={formRef} onSubmit={handleSubmit} isLoading={loading} />
+      </div>
+    </>
   );
 }
 
